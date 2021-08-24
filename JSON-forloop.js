10cf1866f7512beb3c77a6d8a3c51c619f36b77d
Jjlp21 <89472894+Jjlp21@users.noreply.github.com>
2021-08-24 22:37:46 +0530

    Add files via upload

---
+++ JSON-forloop.js
@@ -0,0 +1,22 @@
+var obj = [
+      { 
+        person: "Name-1", 
+        age: "2", 
+        company: "GUVI" 
+      },
+      { 
+        person: "Name-2",
+        age: "5", 
+        company: "GUVI geek" 
+      },
+      { person: "Name-3",
+        age: "8",
+        company: "GUVI geek network" 
+      },
+];
+
+// iterating using basic for loop
+
+for (let store=0;store<obj.length+1;store++){
+   console.log(obj[store]);
+}
